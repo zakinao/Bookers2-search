24d43f5e5f4f09e3bdaab010efef5a4000414e5f
zakinao <zakinaororo@gmail.com>
2021-03-02 06:51:52 +0000

    finish search

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,34 @@
+class SearchController < ApplicationController
+  def search
+    @model = params["search"]["model"]
+    @content = params["search"]["content"]
+    @how = params["search"]["how"]
+    @datas = search_for(@how, @model, @content)
+  end
+  
+  private
+  def match(model, content)
+    if model =="user"
+      User.where(name: content)
+    elsif model == "book"
+      Book.where(title: content)
+    end
+  end
+
+  def partical(model, content)
+    if model =="user"
+      User.where("name LIKE ?", "%#{content}%")
+    elsif model == "book"
+      Book.where("title LIKE ?", "%#{content}%")
+    end
+  end
+  
+  def search_for(how, model, content)
+    case how
+    when 'match'
+      match(model, content)
+    when 'partical'
+      partical(model, content)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/search' => 'search#search'
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
